57be0e6b6f0cce590acd4f06558c1c260d329c66
Shvam Singh <singhshivam583@yahoo.in>
2023-09-10 19:36:49 +0530

    update

---
+++ 9-Camera/camera.js
@@ -0,0 +1,70 @@
+
+// const videoElement = document.getElementById('box');
+// const Button = document.getElementById('control');
+// // const stopButton = document.getElementById('stop-camera');
+
+// var flag=1;
+// let stream;
+// async function camera(){
+// if(flag==1){
+//     Button.addEventListener('click', async() => {
+//         videoElement.innerHTML=`<video style="border-radius:5px;" id="camera-feed" height="370px" width="500px" controls autoplay></video>`
+//         const video=document.getElementById('camera-feed')
+//         // Get access to the user's camera
+//         stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//         video.srcObject = stream;
+//         Button.innerHTML='Close Camera'
+//         flag=0;
+//     });
+// }
+// else(){
+//     Button.addEventListener('click', async() => {
+//         videoElement.innerHTML=``;
+//         const video=document.getElementById('camera-feed')
+//         // Get access to the user's camera
+//         stream = await navigator.mediaDevices.getUserMedia({ video: false });
+//         video.srcObject = stream;
+//         Button.innerHTML='Close Camera'
+//         flag=1;
+//     });
+// }
+// }
+// camera();
+
+const videoElement = document.getElementById('camera-feed');
+const Button = document.getElementById('control');
+// const stopButton = document.getElementById('stop-camera');
+
+let stream;
+
+Button.addEventListener('click', async () => {
+    try {
+        // Get access to the user's camera
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+        // Display the camera feed in the video element
+        videoElement.srcObject = stream;
+
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    } catch (error) {
+        console.error('Error accessing the camera:', error);
+    }
+    Button.innerHTML='Close Camera'
+});
+
+Button.addEventListener('click', () => {
+    if (stream) {
+        // Stop the camera stream and release the camera
+        const tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
+
+        videoElement.srcObject = null;
+
+        startButton.disabled = false;
+        stopButton.disabled = true;
+    }
+    Button.innerHTML='Open Camera'
+});
+
+
