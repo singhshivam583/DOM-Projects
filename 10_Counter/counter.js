cd8f6f323ddb7254287f90e0f0a651f6f514c457
Shvam Singh <singhshivam583@yahoo.in>
2023-09-28 17:23:55 +0530

    update

---
+++ 10_Counter/counter.js
@@ -0,0 +1,75 @@
+
+let count = 0;
+const value = document.querySelector('#value')
+
+
+// const decrease = document.querySelector('.decrease')
+// const reset = document.querySelector('.reset')
+// const increase = document.querySelector('.increase')
+
+// decrease.addEventListener('click', () => {
+//     count--;
+//     if (count > 0) {
+//         value.style.color = "green";
+//     }
+//     if (count < 0) {
+//         value.style.color = "red";
+//     }
+//     if(count ===0 ){
+//         value.style.color="black" ;
+//     }
+//     value.innerHTML = count
+// })
+
+// increase.addEventListener('click', () => {
+//     count++;
+//     if (count > 0) {
+//         value.style.color = "green";
+//     }
+//     if (count < 0) {
+//         value.style.color = "red";
+//     }
+//     if(count ===0 ){
+//         value.style.color="black" ;
+//     }
+//     value.innerHTML = count
+// })
+
+// reset.addEventListener('click', () => {
+//     count = 0;
+//     value.style.color = "black"
+//     value.innerHTML = count
+// })
+
+
+
+//Another Method
+
+const btns = document.querySelectorAll(".btn")
+
+btns.forEach((btn) => btn.addEventListener('click', (e) => {
+    const event = e.target.innerHTML;
+    
+    if(event == 'decrease'){
+        count--;
+    }
+    if(event == 'reset'){
+        count = 0;
+    }
+    if(event == 'increase'){
+        count++;
+    }
+
+    if (count > 0) {
+        value.style.color = "green";
+    }
+    if (count < 0) {
+        value.style.color = "red";
+    }
+    if(count ===0 ){
+        value.style.color="black" ;
+    }
+
+    value.innerHTML = count;
+
+}))
---
+++ 11_ProfileCard/profile.js
@@ -0,0 +1,86 @@
+// local reviews data
+const profile = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+
+  const img = document.getElementById('person-img');
+  const name = document.getElementById('person-name');
+  const job = document.getElementById('person-job');
+  const info = document.getElementById('person-info');
+  
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const randomBtn = document.querySelector('.random-btn');
+  
+  
+  let itemIndex = 0;
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const item = profile[itemIndex];
+    img.src = item.img;
+    name.innerHTML = item.name;
+    job.innerHTML = item.job;
+    info.innerHTML = item.text;
+  });
+
+  function PersonDetails(person) {
+    const item = profile[person];
+    img.src = item.img;
+    name.innerHTML = item.name;
+    job.innerHTML = item.job;
+    info.innerHTML = item.text;
+  }
+ 
+
+  nextBtn.addEventListener('click', () => {
+    itemIndex++;
+    if (itemIndex > profile.length - 1) {
+      itemIndex = 0;
+    }
+    PersonDetails(itemIndex);
+  });
+ 
+
+  prevBtn.addEventListener('click', () => {
+    itemIndex--;
+    if (itemIndex < 0) {
+      itemIndex = profile.length - 1;
+    }
+    PersonDetails(itemIndex);
+  });
+  
+
+
+  randomBtn.addEventListener('click', () => {
+    itemIndex = Math.floor(Math.random() *profile.length);
+
+    PersonDetails(itemIndex);
+  });
+  
